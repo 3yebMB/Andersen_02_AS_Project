df9b3708f68d1c2fa081c060a860973a10362f31
Michael <3yebmb@gmail.com>
2022-02-14 16:36:10 +0400

    add: methods showToast and countUp

--- app/src/main/java/dev/m13d/helloworld/MainActivity.kt
+++ app/src/main/java/dev/m13d/helloworld/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import dev.m13d.helloworld.databinding.ActivityMainBinding
+import android.widget.Toast
 
 private val LOG_TAG = MainActivity::class.java.simpleName
 
@@ -11,14 +12,21 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
+    private var mCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Log.d(LOG_TAG, "Hello World")
+    }
 
-        binding.button.setOnClickListener {
-            throw Exception("I'm exception")
-        }
+    fun showToast(view: android.view.View) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun countUp(view: android.view.View) {
+        mCount++
+        binding.showCount.text = mCount.toString()
     }
 }
