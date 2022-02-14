a08ac4c4bc3843633e00f4b3ba9289e6e734e1dc
Michael <3yebmb@gmail.com>
2022-02-14 14:24:44 +0400

    add: Logger to onCreate method

--- app/src/main/java/dev/m13d/helloworld/MainActivity.kt
+++ app/src/main/java/dev/m13d/helloworld/MainActivity.kt
@@ -2,10 +2,14 @@ package dev.m13d.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private val LOG_TAG = MainActivity::class.java.simpleName
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(LOG_TAG, "Hello World");
     }
 }
