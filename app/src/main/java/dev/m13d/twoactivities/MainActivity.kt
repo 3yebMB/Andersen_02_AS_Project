54b7468122e0de8c0bb355bb5462e0c2396372f8
Michael <3yebmb@gmail.com>
2022-02-16 17:12:06 +0400

    add: button listener in mainactivity class

--- app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
+++ app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
@@ -2,10 +2,17 @@ package dev.m13d.twoactivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private val LOG_TAG = MainActivity::class.java.simpleName
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchSecondActivity(view: android.view.View) {
+        Log.d(LOG_TAG, "Button clicked!")
     }
 }
