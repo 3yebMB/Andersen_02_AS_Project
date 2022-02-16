af7dcfdba3c738b35d9bf5309727956200e4344f
Michael <3yebmb@gmail.com>
2022-02-17 00:47:43 +0400

    update: switch to the new Activity Result API

--- app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
+++ app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
@@ -1,6 +1,5 @@
 package dev.m13d.twoactivities
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -13,36 +12,31 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-
-    fun launchSecondActivity(view: android.view.View) {
-        Log.d(LOG_TAG, "Button clicked!")
-        val intent = Intent(this, SecondActivity::class.java)
-        val message = binding.editTextMain.text.toString()
-        intent.putExtra(Companion.EXTRA_MESSAGE, message)
-        startActivityForResult(intent, TEXT_REQUEST)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                val reply = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
-                with(binding) {
-                    textHeaderReply.visibility = View.VISIBLE
-                    textMessageReply.visibility = View.VISIBLE
-                    textMessageReply.text = reply
-                }
+    private val sendMessage = registerForActivityResult(SecondActivity.Contract()) {
+        if (it != null && it != "") {
+            with(binding) {
+                textHeaderReply.visibility = View.VISIBLE
+                textMessageReply.visibility = View.VISIBLE
+                textMessageReply.text = it
+            }
+        } else {
+            with(binding) {
+                textHeaderReply.visibility = View.INVISIBLE
+                textMessageReply.visibility = View.INVISIBLE
             }
         }
     }
 
-    companion object {
-        const val EXTRA_MESSAGE = "dev.m13d.twoactivities.extra.MESSAGE"
-        const val TEXT_REQUEST = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonMain.setOnClickListener { launchSecondActivity() }
     }
+
+    private fun launchSecondActivity() {
+        Log.d(LOG_TAG, "Button clicked!")
+        sendMessage.launch(binding.editTextMain.text.toString())
+    }
+
 }
--- app/src/main/java/dev/m13d/twoactivities/SecondActivity.kt
+++ app/src/main/java/dev/m13d/twoactivities/SecondActivity.kt
@@ -1,7 +1,10 @@
 package dev.m13d.twoactivities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 import dev.m13d.twoactivities.databinding.ActivitySecondBinding
 
@@ -9,25 +12,45 @@ class SecondActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySecondBinding
 
+    private val resultIntent: Intent
+        get() = Intent().apply {
+            putExtra(EXTRA_REPLY, binding.editTextSecond.text.toString())
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = intent
-        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+        val message = intent.getStringExtra(EXTRA_INPUT)
         binding.textMessage.text = message
+
+        if (message != "")
+            binding.textHeader.visibility = View.VISIBLE
+
+        binding.buttonSecond.setOnClickListener { onSavePressed() }
     }
 
-    fun returnReply(view: android.view.View) {
-        val reply = binding.editTextSecond.text.toString()
-        val replyIntent = Intent()
-        replyIntent.putExtra(EXTRA_REPLY, reply)
-        setResult(RESULT_OK, replyIntent)
+    private fun onSavePressed() {
+        setResult(RESULT_OK, resultIntent)
         finish()
     }
 
+    class Contract : ActivityResultContract<String, String>() {
+
+        override fun createIntent(context: Context, input: String?) =
+            Intent(context, SecondActivity::class.java).apply {
+                putExtra(EXTRA_INPUT, input)
+            }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): String? {
+            if (intent == null) return null
+            return intent.getStringExtra(EXTRA_REPLY)
+        }
+    }
+
     companion object {
+        const val EXTRA_INPUT = "dev.m13d.twoactivities.extra.MESSAGE"
         const val EXTRA_REPLY = "dev.m13d.twoactivities.extra.REPLY"
     }
 }
