57dad51067fd1fb5f33cb20ac3ac9ef3d2b722b6
Michael <3yebmb@gmail.com>
2022-02-17 00:36:00 +0400

    add: getting result from second activity

--- app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
+++ app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
@@ -1,14 +1,17 @@
 package dev.m13d.twoactivities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.content.Intent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import dev.m13d.twoactivities.databinding.ActivityMainBinding
 
 private val LOG_TAG = MainActivity::class.java.simpleName
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +24,25 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SecondActivity::class.java)
         val message = binding.editTextMain.text.toString()
         intent.putExtra(Companion.EXTRA_MESSAGE, message)
-        startActivity(intent)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val reply = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
+                with(binding) {
+                    textHeaderReply.visibility = View.VISIBLE
+                    textMessageReply.visibility = View.VISIBLE
+                    textMessageReply.text = reply
+                }
+            }
+        }
     }
 
     companion object {
         const val EXTRA_MESSAGE = "dev.m13d.twoactivities.extra.MESSAGE"
+        const val TEXT_REQUEST = 1
     }
 }
