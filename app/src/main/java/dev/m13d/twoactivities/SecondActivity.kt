74fc0bc6d5c737c97cc3e2ec112203e1a65c248b
Michael <3yebmb@gmail.com>
2022-02-16 17:18:58 +0400

    add: second activity

---
+++ app/src/main/java/dev/m13d/twoactivities/SecondActivity.kt
@@ -0,0 +1,11 @@
+package dev.m13d.twoactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
