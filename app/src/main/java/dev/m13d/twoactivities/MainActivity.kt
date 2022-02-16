963e42e7b6760645bf7b10ba23c6a85f52ec8569
Michael <3yebmb@gmail.com>
2022-02-16 17:39:10 +0400

    add: put extra into intent & send it to second activity

--- app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
+++ app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.content.Intent
+import dev.m13d.twoactivities.databinding.ActivityMainBinding
 
 private val LOG_TAG = MainActivity::class.java.simpleName
+const val EXTRA_MESSAGE = "dev.m13d.twoactivities.extra.MESSAGE"
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun launchSecondActivity(view: android.view.View) {
         Log.d(LOG_TAG, "Button clicked!")
         val intent = Intent(this, SecondActivity::class.java)
+        val message = binding.editTextMain.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
         startActivity(intent)
     }
 }
