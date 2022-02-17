a6120fbf78d097ad1924275955c46d6b18ce510a
Michael <3yebmb@gmail.com>
2022-02-17 17:45:46 +0400

    add: logger to lifecycle methods of activity

--- app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
+++ app/src/main/java/dev/m13d/twoactivities/MainActivity.kt
@@ -34,6 +34,31 @@ class MainActivity : AppCompatActivity() {
         binding.buttonMain.setOnClickListener { launchSecondActivity() }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "onDestroy")
+    }
+
     private fun launchSecondActivity() {
         Log.d(LOG_TAG, "Button clicked!")
         sendMessage.launch(binding.editTextMain.text.toString())
--- app/src/main/java/dev/m13d/twoactivities/SecondActivity.kt
+++ app/src/main/java/dev/m13d/twoactivities/SecondActivity.kt
@@ -3,10 +3,13 @@ package dev.m13d.twoactivities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 import dev.m13d.twoactivities.databinding.ActivitySecondBinding
+
+private val LOG_TAG = SecondActivity::class.java.simpleName
 
 class SecondActivity : AppCompatActivity() {
 
@@ -31,9 +34,35 @@ class SecondActivity : AppCompatActivity() {
         binding.buttonSecond.setOnClickListener { onSavePressed() }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "onDestroy")
+    }
+
     private fun onSavePressed() {
         setResult(RESULT_OK, resultIntent)
         finish()
+        Log.d(LOG_TAG, "End SecondActivity")
     }
 
     class Contract : ActivityResultContract<String, String>() {
