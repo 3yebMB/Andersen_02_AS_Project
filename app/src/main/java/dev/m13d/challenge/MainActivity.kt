85387d390a87d47177ad54516f2cfd46360e09d4
Michael <3yebmb@gmail.com>
2022-02-17 15:12:16 +0400

    add: send & get intent from main to second activity

--- app/src/main/java/dev/m13d/challenge/MainActivity.kt
+++ app/src/main/java/dev/m13d/challenge/MainActivity.kt
@@ -1,11 +1,39 @@
 package dev.m13d.challenge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import dev.m13d.challenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onClickButton(view: android.view.View) {
+        var res = 0
+        when(view.id) {
+            R.id.button1 -> res = R.string.passage_1
+            R.id.button2 -> res = R.string.passage_2
+            R.id.button3 -> res = R.string.passage_3
+        }
+        launchSecondActivity(res)
+    }
+
+    private fun launchSecondActivity(res: Int) {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra(EXTRA_MESSAGE, res)
+        startActivity(intent)
+    }
+
+    companion object {
+        const val EXTRA_MESSAGE = "dev.m13d.challenge.MESSAGE"
     }
 }
--- app/src/main/java/dev/m13d/challenge/SecondActivity.kt
+++ app/src/main/java/dev/m13d/challenge/SecondActivity.kt
@@ -2,10 +2,19 @@ package dev.m13d.challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.m13d.challenge.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = intent
+        val res = intent.getIntExtra(MainActivity.EXTRA_MESSAGE, 0)
+        binding.textView.text = getString(res)
     }
 }
