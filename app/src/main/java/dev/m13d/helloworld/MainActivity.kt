59db47af66cf1add15acbaad672f2445c8d52019
Michael <3yebmb@gmail.com>
2022-02-18 03:24:02 +0400

    add: save & restore value in bundle

--- app/src/main/java/dev/m13d/helloworld/MainActivity.kt
+++ app/src/main/java/dev/m13d/helloworld/MainActivity.kt
@@ -1,15 +1,33 @@
 package dev.m13d.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-
-private val LOG_TAG = MainActivity::class.java.simpleName
+import androidx.appcompat.app.AppCompatActivity
+import dev.m13d.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private var mCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d(LOG_TAG, "Hello World");
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonIncrease.setOnClickListener {
+            mCount++
+            binding.tvCounter.text = mCount.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("COUNTER", mCount)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+            binding.tvCounter.text = savedInstanceState.getInt("COUNTER", 0).toString()
     }
 }
